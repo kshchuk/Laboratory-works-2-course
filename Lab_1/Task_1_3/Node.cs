c5d008436c9149af27349c80c66661ff8c3dce9e
kshchuk <kshchuk@gmail.com>
2022-09-15 23:56:44 +0300

    Created Node for single linked list

---
+++ Lab_1/Task_1_3/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1_3
+{
+    internal sealed class Node<T>
+    {
+        public T data;
+        public Node<T>? next, prev;
+
+        Node(T data)
+        {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+}
