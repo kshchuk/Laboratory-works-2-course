bea40b0bbd2c8241d501fd9b9c9ae92571bf6866
kshchuk <kshchuk@gmail.com>
2022-09-16 00:59:05 +0300

    Increment and decrement operators overloading

--- Lab_1/Task_1_3/Node.cs
+++ Lab_1/Task_1_3/Node.cs
@@ -8,14 +8,25 @@ namespace Task_1_3
 {
     internal sealed class Node<T>
     {
-        public T data;
-        public Node<T>? next, prev;
+        public T Data { get; set; }
+        public Node<T> next, prev;
 
         public Node(T data)
         {
-            this.data = data;
+            this.Data = data;
             this.next = null;
             this.prev = null;
+        }
+
+        public static Node<T> operator ++(Node<T> node)
+        {
+            node = node.next;
+            return node;
+        }
+        public static Node<T> operator --(Node<T> node)
+        {
+            node = node.prev;
+            return node;
         }
     }
 }
