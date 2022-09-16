a63e828895d9e9bc74f597329e2bab23c0af6ac7
kshchuk <kshchuk@gmail.com>
2022-09-17 01:11:00 +0300

    Implemented interfaces: ICollection<T>, IEnumerable<T>, IEnumerator<T>, ICollection Methods: AddFirst, AddLast, AddBefore, AddAfter

---
+++ Lab_1/Task_1_3/MyLinkedList.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Task_1_3
+{
+    public class MyLinkedList<T> : ICollection<T>, IEnumerable<T>, IEnumerator<T>, ICollection
+    {   
+        protected Node<T> head;
+
+        public MyLinkedList()
+        {
+            head = null;
+        }
+
+        // Implementing ICollection<T>, ICollection interfaces
+        public int Count { get; private set; } = 0;
+        public bool IsReadOnly => false;
+        public bool IsSynchronized => false;
+        public object SyncRoot { get; } = new object();
+        public void Add(T item)
+        {
+            if (head == null)
+            {
+                head = new Node<T>(item);
+                return;
+            }
+            else
+            {
+                Node<T> node = head;
+                while (node.Next != null) node = node.Next;
+
+                node.Next = new Node<T>(item);
+            }
+            Count++;
+        }
+        public void Clear()
+        {
+            Node<T> current = head;
+            while(current != null)
+            {
+                Node<T> temp = current;
+                current = current.Next;
+                temp.Invalidate();
+            }
+            Count = 0;
+        }
+        public bool Remove(T item)
+        {
+            Node<T> current = head;
+            if (current != null)
+            {
+                if (current.Value.Equals(item))
+                {
+                    head = head.Next;
+                    current.Invalidate();
+                    return true;
+                }
+                while (current.Next != null)
+                {
+                    if (current.Next.Value.Equals(item))
+                    {
+                        Node<T> temp = current.Next;
+                        current.Next = current.Next.Next;
+                        temp.Invalidate();
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        public bool Contains(T item)
+        {
+            Node<T> current = head;
+            if (current != null)
+            {
+                while (current.Next != null)
+                {
+                    if (current.Next.Equals(item))
+                        return true;
+                }
+            }
+            return false;                       
+        }
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            Node<T> current = head;
+            while (current != null)
+            {
+                array[arrayIndex++] = current.Value;
+                current = current.Next;
+            }
+        }
+        public void CopyTo(Array array, int index)
+        {
+            Node<T> current = head;
+            while (current != null)
+            { 
+                array.SetValue(current.Value, index++);
+            }
+        }
+
+        public void AddFirst(T item)
+        {
+            if (head == null)
+                head = new Node<T>(item);
+            else
+            {
+                Node<T> node = new Node<T>(item);
+                node.Next = head;
+                head = node;
+            }
+            Count++;
+        }
+        public void AddLast(T item)
+        {
+            if (head == null)
+                head = new Node<T>(item);
+            else
+            {
+                Node<T> node = new Node<T>(item);
+                Node<T> current = head;
+                while (current.Next != null) current = current.Next;
+                current.Next = node;
+            }
+            Count++;
+        }
+        public void AddBefore(T item)
+        {
+            if (head == null)
+                throw new ArgumentException("Item does not exist in the list");
+            else 
+            if (head.Value.Equals(item))
+            {
+                AddFirst(item);
+            }
+            else
+            {
+                Node<T> current = head.Next;
+                while (current.Next != null)
+                {
+                    if (current.Next.Value.Equals(item))
+                    {
+                        Node<T> node = new Node<T>(item);
+                        node.Next = current.Next;
+                        current.Next = node;
+                        Count++;
+                        return;
+                    }
+                }
+                // Item did not find
+                throw new ArgumentException("Item does not exist in the list");
+            }
+        }
+        public void AddAfter(T item)
+        {
+            if (head == null)
+                throw new ArgumentException("Item does not exist in the list");
+            else
+            {
+                Node<T> current = head;
+                while (current != null)
+                {
+                    if (current.Value.Equals(item))
+                    {
+                        Node<T> node = new Node<T>(item);
+                        node.Next = current.Next;
+                        current.Next = node;
+                        Count++;
+                        return;
+                    }
+                }
+                // Item did not find
+                throw new ArgumentException("Item does not exist in the list");
+            }
+        }
+
+        // Implementing IEnumerable<T>, IEnumerator<T> interfaces
+        private Node<T> current;
+        public T Current => current.Value;
+        object IEnumerator.Current => current.Value;
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node<T> current = head;
+            while (current!=null)
+            {
+                yield return current.Value;
+                current = current.Next;
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this as IEnumerator;
+        }
+        public bool MoveNext()
+        {
+            if (current.Next != null)
+            {
+                current = current.Next;
+                return true;
+            }
+            else
+                return false;
+        }
+        public void Reset()
+        {
+            current = head;
+        }
+
+        // Implementing IDisposible interface
+        public void Dispose()
+        {
+        }
+    }
+}
