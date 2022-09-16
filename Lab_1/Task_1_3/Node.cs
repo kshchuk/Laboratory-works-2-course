c1f67388ee2cf773a8fd4fc72fc3138f1b094caa
kshchuk <kshchuk@gmail.com>
2022-09-16 19:46:34 +0300

    Class renamed to Node

--- Lab_1/Task_1_3/Node.cs
+++ Lab_1/Task_1_3/Node.cs
@@ -10,25 +10,25 @@ namespace Task_1_3
     /// Node for Single Linked List
     /// </summary>
     /// <typeparam name="T"></typeparam>
-    public sealed class LinkedListNode<T>
+    public sealed class Node<T>
     {
         internal MyLinkedList<T> list;
-        internal LinkedListNode<T> next;
-        internal LinkedListNode<T> prev;
+        internal Node<T> next;
+        internal Node<T> prev;
         internal T item;
 
-        public LinkedListNode(T value)
+        public Node(T value)
         {
             this.item = value;
         }
-        internal LinkedListNode(MyLinkedList<T> list, T value)
+        internal Node(MyLinkedList<T> list, T value)
         {
             this.list = list;
             this.item = value;
         }
 
         public MyLinkedList<T> List => list;
-        public LinkedListNode<T> Next => next == null || next == list.head ? null : next;
+        public Node<T> Next => next == null || next == list.head ? null : next;
         public T Value
         {
             get { return item; }
