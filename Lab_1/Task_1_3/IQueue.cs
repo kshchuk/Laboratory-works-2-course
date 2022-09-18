b81259fbf10a8a405881aa97d9c33cab5cb801c1
kshchuk <kshchuk@gmail.com>
2022-09-18 16:02:44 +0300

    Created queue interface

---
+++ Lab_1/Task_1_3/IQueue.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1_3
+{
+    interface IQueue<T>
+    {
+        /// <summary>
+        /// Check whether the queue is empty
+        /// </summary>
+        /// <returns></returns>
+        bool isEmpty();
+        /// <summary>
+        /// Adds a new element to the rear end of the queue
+        /// </summary>
+        /// <param name="item"> Element to add</param>
+        void Enqueue(T item);
+        /// <summary>
+        /// Removes and returns the object at the beginning of the queue
+        /// </summary>
+        /// <returns></returns>
+        T Dequeue();
+        /// <summary>
+        /// Returns the object at the beginning of the queue without removing it.
+        /// </summary>
+        /// <returns></returns>
+        T Peek();
+    }
+}
