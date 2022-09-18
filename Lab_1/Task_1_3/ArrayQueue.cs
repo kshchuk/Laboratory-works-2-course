476e570ce362e7106432c05059e517b95aca88ec
kshchuk <kshchuk@gmail.com>
2022-09-18 17:05:13 +0300

    Implemented queue based on array

--- Lab_1/Task_1_3/ArrayQueue.cs
+++ Lab_1/Task_1_3/ArrayQueue.cs
@@ -8,24 +8,56 @@ namespace Task_1_3
 {
     public class ArrayQueue<T> : IQueue<T>
     {
+        private int front, end;
+        private readonly int size;
+        private T[] queue;
+
+        public ArrayQueue(int size)
+        {
+            this.size = size;
+            queue = new T[size];
+            front = 0;
+            end = 0;
+        }
         public T Dequeue()
         {
-            throw new NotImplementedException();
-        }
+            if (isEmpty())
+                throw new InvalidOperationException("Queue is empty");
+            else
+            {
+                T data = queue[front];
+                for (int i = 0; i < end - 1; i++)
+                {
+                    queue[i] = queue[i + 1];
+                }
 
+                if (end < size)
+                    queue[end] = default(T);
+
+                end--;
+                return data;
+            }   
+        }
         public void Enqueue(T item)
         {
-            throw new NotImplementedException();
+            if (size == end)
+            {
+                throw new OverflowException("Queue is full");
+            }
+            else
+            {
+                queue[end] = item;
+                end++;
+            }
+            return;
         }
-
-        public bool isEmpty()
-        {
-            throw new NotImplementedException();
-        }
-
+        public bool isEmpty() => front == end;
         public T Peek()
         {
-            throw new NotImplementedException();
+            if (isEmpty())
+                throw new InvalidOperationException("Queue is empty");
+            else
+                return queue[front];
         }
     }
 }
