30ee833f1a53748cc18903a1f350d6897235605f
kshchuk <kshchuk@gmail.com>
2022-09-18 18:57:47 +0300

    Implemented deque based on double linked list

---
+++ Lab_1/Task_1_3/IDeque.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1_3
+{
+    interface IDeque<T>
+    {
+        void PushBack(T item);
+        T PopBack();
+        void PushFront(T item);
+        T PopFront();
+        T PeekBack();
+        T PeekFront();
+        bool isEmpty();
+    }
+}
