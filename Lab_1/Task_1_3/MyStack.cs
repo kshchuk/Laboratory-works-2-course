0e7e7bc2f027e275e1c48ba870564c588abfac6b
kshchuk <kshchuk@gmail.com>
2022-09-18 13:18:52 +0300

    Created interface of stack

---
+++ Lab_1/Task_1_3/MyStack.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1_3
+{
+    interface MyStack<T>
+    {
+        int Count { get; set; }
+
+        /// <summary>
+        /// Deletes all elements
+        /// </summary>
+        void Clear();
+        /// <summary>
+        /// Checks whether there is no elemets in the stack
+        /// </summary>
+        /// <returns></returns>
+        bool isEmppy();
+        /// <summary>
+        /// Inserts an object as the top element of the stack
+        /// </summary>
+        /// <param name="item"></param>
+        void Push(T item);
+        /// <summary>
+        /// Gets top element and removes him
+        /// </summary>
+        T Pop();
+        /// <summary>
+        /// Gets top element without removing him
+        /// </summary>
+        T Peek();
+    }
+}
